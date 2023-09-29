0a333c0f6e27b5050ed4ffaa51c5b9c7a39ccf37
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2023-09-29 10:31:58 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import coffee_machine as Coffee
+
+#1- 3 hot flavors
+#2- espresso, latte, cappuccino
+#3- espresso: 50ml w, 18g coffee; latte: 200ml w, 24g coffee, 150ml milk; cupp: 250ml w, 24g coffee, 100ml milk
+#4- tank: 300ml w, 200ml milk, 100g coffee
+#5- report: checks available resources
+#6- check resources are sufficient
+#7- coin operated
+#8- process coins and check for successful transaction
+#9- successful transaction> check available resources
+#10- if resources available make coffee and deduct resources
+#11- espresso 1$, latte 2.50$, cuppuccino 3.5$
+#12- quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01

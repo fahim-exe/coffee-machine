272b434963442bf587e1a61eee157d6ee5b470f7
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2023-09-29 10:52:55 +0600

    written all requirements

---
+++ coffee_machine.py
@@ -0,0 +1,8 @@
+def machine(user_input:str, money=None):
+    
+
+    if user_input=="report":
+        print()
+
+
+    
--- main.py
+++ main.py
@@ -12,3 +12,34 @@ import coffee_machine as Coffee
 #10- if resources available make coffee and deduct resources
 #11- espresso 1$, latte 2.50$, cuppuccino 3.5$
 #12- quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+
+tank = {'water': 300, 'milk':200, 'coffee':100, 'money':0}
+
+while True:
+    user_input = input("What would you like? \nEspresso: 1$ \nLatte: 2.50$ \nCuppuccino:3.5$ \nPlease select your coffee:").lower()
+
+    if user_input=="off":
+        exit()
+
+    if user_input=="report":
+        print(f"Water: {tank['water']}ml")
+        print(f"Milk: {tank['milk']}ml")
+        print(f"Coffee: {tank['coffee']}")
+        print(f"Money: ${tank['money']}")
+        print()
+    
+    if user_input=="latte":
+        pass
+
+    if user_input=="cuppuccino":
+        pass
+
+    if user_input=="espresso":
+        pass
+
+    if user_input=="reset":
+        pass
+
+    if user_input=="refill":
+        pass
+         

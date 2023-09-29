98e9fc3d30e90217562f49de5dcadf0fc066771f
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2023-09-29 15:15:42 +0600

    completed the internal functions

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,8 +1,16 @@
-def machine(user_input:str, money=None):
-    
+def calculate():
+    pass
 
-    if user_input=="report":
-        print()
-
-
+def check_resources(res:dict, flavor:str):
+    if flavor=="espresso":
+        if res["water"]>50 and res['coffee']>18:
+            return True
+        
+    if flavor=="latte":
+        if res['water']>200 and res["coffee"]>24 and res["milk"]>150:
+            return True
+        
+    if flavor=="cappuccino":
+        if res['water']>250 and res["coffee"]>24 and res["milk"]>100:
+            return True
     
--- main.py
+++ main.py
@@ -10,36 +10,160 @@ import coffee_machine as Coffee
 #8- process coins and check for successful transaction
 #9- successful transaction> check available resources
 #10- if resources available make coffee and deduct resources
-#11- espresso 1$, latte 2.50$, cuppuccino 3.5$
+#11- espresso 0.5$, latte 1.50$, cuppuccino 2.5$
 #12- quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
 
 tank = {'water': 300, 'milk':200, 'coffee':100, 'money':0}
+internal = ["report", "off", "reset", "refill"]
+menu = ["latte", "cappuccino", "espresso"]
 
 while True:
     user_input = input("What would you like? \nEspresso: 1$ \nLatte: 2.50$ \nCuppuccino:3.5$ \nPlease select your coffee:").lower()
+    print()
 
-    if user_input=="off":
-        exit()
+    if user_input in internal:
 
-    if user_input=="report":
-        print(f"Water: {tank['water']}ml")
-        print(f"Milk: {tank['milk']}ml")
-        print(f"Coffee: {tank['coffee']}")
-        print(f"Money: ${tank['money']}")
-        print()
-    
+        if user_input=="off":
+            exit()
+            break
+
+        if user_input=="report":
+            print(f"Water: {tank['water']}ml")
+            print(f"Milk: {tank['milk']}ml")
+            print(f"Coffee: {tank['coffee']}g")
+            print(f"Money: ${tank['money']}")
+            print()
+
+        if user_input=="reset":
+            tank = {'water': 300, 'milk':200, 'coffee':100, 'money':0}
+            print()
+            continue
+
+        if user_input=="refill":
+            
+            chk = input("Refill item (r) or Full refill (f): ").lower()
+            if chk=="r":
+                fill_item = input("Select one Coffee(c), Water(w), Milk(m): ")
+                fill = int(input("Enter amount: "))
+                if fill_item=="w":
+                    tank['water'] = fill
+                if fill_item=="m":
+                    tank['milk'] = fill
+                if fill_item=="c":
+                    tank['coffee'] = fill
+            
+
+                
+
+
+            else:
+
+                while True:
+
+                    w = int(input("Please enter water quantity: "))
+                    m = int(input("Please enter milk quantity: "))
+                    c = int(input("Please enter coffee quantity: "))
+
+                    if w>300:
+                        print("Please lessen water!!!")
+                        continue
+
+                    if m>200:
+                        print("Please lessen milk!!!")
+                        continue
+
+                    if c>100:
+                        print("Please lessen coffee!!!")
+                        continue
+
+                    if w>0 and w<301:
+                        tank["water"] = w
+                    if m>0 and m<201:
+                        tank["milk"] = m
+                    if c>0 and c<101:
+                        tank["coffee"] = c
+                    
+                    print()
+                    break
+
+    if user_input in menu:
+        q = int(input("How many quarters?: ")) #0.25
+        d = int(input("How many dimes?: ")) #0.10
+        n = int(input("How many nickels?: ")) #0.05
+        p = int(input("How many pennies?: ")) #0.01
+
+        paid = (q*0.25)+(d*0.10)+(n*0.05)+(p*0.01)
+        
+        
+
+
+        if paid>=0.50 and user_input=="espresso":
+            change = round((paid-0.50), 2)
+            check = Coffee.check_resources(tank, user_input)
+            if check:
+                tank["money"] = tank['money']+1
+                tank['water'] = tank["water"]-50
+                tank["coffee"] = tank["coffee"]-18
+
+                if change>0:
+                    print(f"Here is ${change} in change.")
+
+                
+
+                print("Here is your latte... Enjoy!!!")
+                print("")
+
+        if paid>=1.5 and user_input=="latte":
+            change = paid-1.5
+            check = Coffee.check_resources(tank, user_input)
+            if check:
+                tank["money"] = tank['money']+1
+                tank['water'] = tank["water"]-50
+                tank["coffee"] = tank["coffee"]-18
+                tank["milk"] = tank["milk"]-150
+
+                if change>0:
+                    print(f"Here is ${change} in change.")
+
+            
+
+        if paid>=2.5 and user_input=="cappuccino":
+            change = paid-3.5
+            check = Coffee.check_resources(tank, user_input)
+            if check:
+                tank["money"] = tank['money']+1
+                tank['water'] = tank["water"]-50
+                tank["coffee"] = tank["coffee"]-18
+
+                if change>0:
+                    print(f"Here is ${change} in change.")
+
+            
+
+
+
+
+
+        
+        
+
+        
+        
+
     if user_input=="latte":
         pass
 
     if user_input=="cuppuccino":
         pass
 
-    if user_input=="espresso":
-        pass
+    
 
-    if user_input=="reset":
-        pass
+    
 
-    if user_input=="refill":
-        pass
+    
+
+    if user_input not in (internal+menu):
+        print("Please enter a valid input!!!")
+        print()
+
          
